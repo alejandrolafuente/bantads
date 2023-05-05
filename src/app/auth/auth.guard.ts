b81651515480c5926fd60effca3ade9fcdeefffb
Gabriel Ribeiro <gabriel.ribeiro@vmtecnologia.io>
2023-05-05 19:59:33 -0300

    angular

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from './services/login.service';
 import { Router } from '@angular/router';
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private loginService: LoginService,
